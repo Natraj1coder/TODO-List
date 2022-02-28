abb7204b98d88d4448e4cc7990f8cd3de506ed8c
NATRAJ N <natrajnatraj041@gmail.com>
2022-02-28 18:27:16 +0530

    Added git ignore,procfile

--- app.js
+++ app.js
@@ -124,8 +124,12 @@ if(!err){
 
 
 });
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
 
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server started Sucessfully");
 });

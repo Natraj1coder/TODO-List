29d78ea478d88125cadab03e5c8117f445d1bbc4
NATRAJ N <natrajnatraj041@gmail.com>
2022-02-28 19:04:41 +0530

    working

--- app.js
+++ app.js
@@ -124,12 +124,8 @@ if(!err){
 
 
 });
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
-app.listen(port);
 
-app.listen(port, function() {
+
+app.listen(3000, function() {
   console.log("Server started Sucessfully");
 });
